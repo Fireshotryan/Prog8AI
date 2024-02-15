9336ba539f6238b00d7946a51aafd3038e077063
Ryan de Man <Ryan.m.de.man@gmail.com>
2024-02-15 16:15:50 +0100

    het was niet grappig

--- server/server.js
+++ server/server.js
@@ -1,2 +1,13 @@
+import { ChatOpenAI } from "@langchain/openai"
+
+const model = new ChatOpenAI({
+    azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+    azureOpenAIApiVersion: process.env.OPENAI_API_VERSION,
+    azureOpenAIApiInstanceName: process.env.INSTANCE_NAME,
+    azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
+})
+
+const joke = await model.invoke("Tell me a Javascript joke!")
+console.log(joke.content)
 console.log("hello world")
 console.log(process.env.AZURE_OPENAI_API_KEY)
